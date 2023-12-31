2e6685c064ed9ca7adeefb6cd32600e8099123b0
Janko Novakovic <licardian9996@gmail.com>
2023-12-31 15:23:07 +0100

    add JavaScript to Mobile Menu

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,16 @@
-console.log("hello");
+
+const mobileMenuBtn = document.querySelector(".mobile_menu");
+const mobileNavMenu = document.querySelector(".nav_menu");
+const navLinks = document.querySelectorAll(".nav_list li a");
+
+mobileMenuBtn.addEventListener("click", () => {
+    
+    mobileNavMenu.classList.toggle("active");
+
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        mobileNavMenu.classList.remove("active");
+    })
+});
